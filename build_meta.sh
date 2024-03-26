14916d5bc84ee733b58428e7e4bb66e5f05655ed
llh730 <2597763106@qq.com>
2024-03-26 18:08:33 +0800

    support run in qemu-system-loongarch32

--- build_meta.sh
+++ build_meta.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export DEVICE_TREE=la32rmega_demo
+export DEVICE_TREE=la32rsoc_demo
 export ARCH=la32r
 export CROSS_COMPILE=../loongson-gnu-toolchain-8.3-x86_64-loongarch32r-linux-gnusf-v2.0/bin/loongarch32r-linux-gnusf-
 #clear && make la32rmega_defconfig && make && loongarch32r-linux-gnusf-objdump -S u-boot > u-boot.S
